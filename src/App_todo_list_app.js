989b3bf3d8931d52ce86e8f2fd9bf9e6e72923ca
Bwogi Andrew <andrew.bwogi@gmail.com>
2021-12-30 20:44:08 -0800

    useEffect hook added

---
+++ src/App_todo_list_app.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+// import Product from './Components/Product';
+import styles from './App.module.css';
+
+const App = () => {
+	const [todos, setTodos] = useState([]);
+	const [input, setInput] = useState('');
+
+	const addTodo = (e) => {
+		e.preventDefault();
+		setTodos([input, ...todos]); // new items on top then '...todos' later
+		// setTodos([...todos, input]); // '...todos' then new items later
+		setInput('');
+	};
+	return (
+		<div>
+			<h1>Welcome to my TODO List</h1>
+			<form>
+				<input
+					value={input}
+					onChange={(e) => setInput(e.target.value)}
+					type='text'
+				/>
+				<button type='submit' onClick={addTodo}>
+					Add Todo
+				</button>
+			</form>
+
+			<h2>List of Todos</h2>
+			{todos.map((todo) => (
+				<p>{todo}</p>
+			))}
+
+			{/* <p className={styles.error}>error occured!</p> */}
+			{/* <Product
+				name='iPhone watch'
+				description='The best watch ever'
+				price={605.99}
+			/>
+			<Product
+				name='iPhone 13 Pro Max'
+				description='The best phone ever'
+				price={1605.99}
+			/>
+			<Product
+				name='MacBook Pro'
+				description='The best computer ever'
+				price={3605.99}
+			/> */}
+		</div>
+	);
+};
+
+export default App;
---
+++ src/Components/Joke/index.js
@@ -0,0 +1,11 @@
+import styles from './Joke.module.css';
+
+const Joke = () => {
+	return (
+		<div>
+			<p className={styles.joke}>the joke</p>
+		</div>
+	);
+};
+
+export default Joke;
